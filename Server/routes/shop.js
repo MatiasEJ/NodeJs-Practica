8bb44850714c6993efd3bd69ac2f4de2138d7a6a
keta <don.juncos@gmail.com>
2019-09-17 12:56:01 -0300

    Pugeando como loco

--- Server/routes/shop.js
+++ Server/routes/shop.js
@@ -10,7 +10,7 @@ router.get('/',(req, res, next) => {
     // console.log('shop.js',adminData.products);
     // res.sendFile(path.join(rootDir,'views','shop.html'));
     const products = adminData.products;
-    res.render('shop',{prods: products, docTitle: 'shop'});
+    res.render('shop',{prods: products, pageTitle: 'shop', path:'/shop'});
 
     
 
