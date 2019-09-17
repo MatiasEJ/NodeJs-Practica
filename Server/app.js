a7f3ad6949e42633e6bdb51eae3be20da2741af6
keta <don.juncos@gmail.com>
2019-09-17 12:43:57 -0300

    Pugs

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,35 @@
-const http = require("http");
+// const http = require('http'); //creacion server
+// const route = require('./routes');
+// console.log(route.someText);
+const path = require('path');
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express(); 
+app.set('view engine','pug');
+app.set('views','views');
 
 
-const server = http.createServer((req,res)=>{
-    console.log(req);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).render('404');
 });
 
-server.listen(3000, );
 
+
+
+
+//Creacion server
+// const server = http.createServer(app);
+// server.listen(3000);
+
+app.listen(3000);
