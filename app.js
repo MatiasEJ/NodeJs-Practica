dd8234cd811943d681505a0d80409a749e7e4d83
keta <don.juncos@gmail.com>
2020-03-30 19:39:33 -0300

    error handling

--- app.js
+++ app.js
@@ -13,18 +13,38 @@ const bodyParser = require('body-parser');
 const MongoDBStore = require('connect-mongodb-session')(session)
 const csrf = require('csurf');
 const flash = require('connect-flash');
-
+const multer = require('multer');
 const store = new MongoDBStore({
     uri: process.env.DIR_MONGO, 
     collection: 'sessions'
 })
 const csrfProtection = csrf();
-
 app.use(flash());
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb )=>{
+        cb(null, 'images')
+    }, 
+    filename: (req, file, cb)=>{
+        cb(null, new Date().toISOString + '-' + file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb)=>{
+    if ( file.mimetype === 'image/png' ||
+         file.mimetype === 'image/jpg' || 
+         file.mimetype === 'image/jpeg'  ){
+        cb(null, true)
+    }else{
+        cb(null, false)
+    }
+}
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* SERVER */
--- controlers/admin.js
+++ controlers/admin.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product',{
+    res.render('admin/edit-product',{
         pageTitle: 'Add Product', 
         path: '/admin/add-product',
         editing: false,
@@ -14,30 +14,30 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct =  (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
+    const imageUrl = req.file;
     const price = req.body.price;
     const description = req.body.description;
-    
+    console.log(imageUrl)
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
+        console.table(errors.array());
+        return res
+        .status(422)
+        .render('admin/edit-product', {
+            path: '/admin/add-product',
+            pageTitle: 'Add Product',
+            editing: false, 
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array(),
+            product:{
+                title:title,
+                price:price,
+                description:description,
+                imageUrl:imageUrl
+            }
 
-    return res
-    .status(422)
-    .render('admin/edit-product', {
-        path: '/admin/edit-product',
-        pageTitle: 'Add Product',
-        editing: false, 
-        errorMessage: errors.array()[0].msg,
-        validationErrors: errors.array(),
-        product:{
-            title:title,
-            price:price,
-            description:description,
-            imageUrl:imageUrl
-        }
-
-    });
+        });
     }
 
     const product = new Product({
@@ -58,8 +58,7 @@ exports.postAddProduct =  (req, res, next) => {
         const error = new Error(err);
         error.httpStatusCode = 500;
         return next(error);
-        }
-      );
+        });
     
 };
 
